bfd14c6f0e45fbc4a7f4f1e8cc665b0c42113b60
Volodymyr Shpynta <vshpynta@cogniance.com>
2016-02-19 01:17:09 +0200

    Non functional changes

--- src/main/java/fr/xebia/cascading/learn/level2/Mapping.java
+++ src/main/java/fr/xebia/cascading/learn/level2/Mapping.java
@@ -15,55 +15,54 @@ import cascading.tuple.Fields;
  * {@link Each} will allow you to execute a function for all inputs, a bit like a for loop.
  */
 public class Mapping {
-	
-	/**
-	 * Use a {@link Each} in order to {@link Filter} out "line"s which do not contains "Hadoop".
-	 * {@link ExpressionFilter} makes it very easy for simple case.
-	 * 
-	 * source field(s) : "line"
-	 * sink field(s) : "line"
-	 * 
-	 * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#each-every
-	 */
-	public static FlowDef filterWithExpression(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
-		return null;
-	}
-	
-	/**
-	 * Use a {@link Each} in order to transform every "line" in lowercase.
-	 * {@link Function} allows to change the input, and again 
-	 * {@link ExpressionFunction} is really helpful for simple case.
-	 * 
-	 * source field(s) : "line"
-	 * sink field(s) : "line"
-	 *
-	 * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#each-every
-	 */
-	public static FlowDef transformWithExpression(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
-		return null;
-	}
-	
-	/**
-	 * Split each "line" into "word"s using a custom {@link Function}.
-	 * The provided function do only the identity.
-	 * 
-	 * input field(s) : "line"
-	 * output field(s) : "word"
-	 * 
-	 * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#each-every
-	 * 
-	 * Of course, for a real use case, regular expressions could be used with existent functions
-	 * @see http://docs.cascading.org/cascading/3.0/userguide/ch16-operations.html#_regular_expression_operations
-	 */
-	public static FlowDef transformWithACustomFunction(Tap<?, ?, ?> source,
-			Tap<?, ?, ?> sink) {
-		Pipe pipe = new Each("split", new Fields("line"), new CustomSplitFunction<Object>(new Fields("word")), Fields.SWAP);
-		return FlowDef.flowDef()//
-				.addSource(pipe, source) //
-				.addTail(pipe)//
-				.addSink(pipe, sink);
-	}
-	
-	
+
+    /**
+     * Use a {@link Each} in order to {@link Filter} out "line"s which do not contains "Hadoop".
+     * {@link ExpressionFilter} makes it very easy for simple case.
+     * <p/>
+     * source field(s) : "line"
+     * sink field(s) : "line"
+     *
+     * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#each-every
+     */
+    public static FlowDef filterWithExpression(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
+        return null;
+    }
+
+    /**
+     * Use a {@link Each} in order to transform every "line" in lowercase.
+     * {@link Function} allows to change the input, and again
+     * {@link ExpressionFunction} is really helpful for simple case.
+     * <p/>
+     * source field(s) : "line"
+     * sink field(s) : "line"
+     *
+     * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#each-every
+     */
+    public static FlowDef transformWithExpression(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
+        return null;
+    }
+
+    /**
+     * Split each "line" into "word"s using a custom {@link Function}.
+     * The provided function do only the identity.
+     * <p/>
+     * input field(s) : "line"
+     * output field(s) : "word"
+     *
+     * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#each-every
+     * <p/>
+     * Of course, for a real use case, regular expressions could be used with existent functions
+     * @see http://docs.cascading.org/cascading/3.0/userguide/ch16-operations.html#_regular_expression_operations
+     */
+    public static FlowDef transformWithACustomFunction(Tap<?, ?, ?> source,
+                                                       Tap<?, ?, ?> sink) {
+        Pipe pipe = new Each("split", new Fields("line"), new CustomSplitFunction<Object>(new Fields("word")), Fields.SWAP);
+        return FlowDef.flowDef()//
+                .addSource(pipe, source) //
+                .addTail(pipe)//
+                .addSink(pipe, sink);
+    }
+
 
 }
