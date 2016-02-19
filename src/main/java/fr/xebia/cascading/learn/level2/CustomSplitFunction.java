526fa8c1a26c6a49835afdcd8a4d81ee722b12f1
VolodymyrShpynta <vshpynta@cogniance.com>
2016-02-19 14:25:43 +0200

    Fixed MappingTest tests

--- src/main/java/fr/xebia/cascading/learn/level2/CustomSplitFunction.java
+++ src/main/java/fr/xebia/cascading/learn/level2/CustomSplitFunction.java
@@ -8,19 +8,19 @@ import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 
 public class CustomSplitFunction<Context> extends BaseOperation<Context> implements Function<Context> {
-	private static final long serialVersionUID = 1L;
-	
-	public CustomSplitFunction(Fields wordField) {
-		super(1, wordField);
-	}
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public void operate(@SuppressWarnings("rawtypes") FlowProcess flowProcess,
-			FunctionCall<Context> functionCall) {
-		String line = functionCall.getArguments().getString(0);
-		String word = line; // or not ?
-		functionCall.getOutputCollector().add(new Tuple(word));
-		functionCall.getOutputCollector().add(new Tuple(word));
-	}
-	
+    public CustomSplitFunction(Fields wordField) {
+        super(1, wordField);
+    }
+
+    @Override
+    public void operate(@SuppressWarnings("rawtypes") FlowProcess flowProcess,
+                        FunctionCall<Context> functionCall) {
+        String line = functionCall.getArguments().getString(0);
+        for (String word : line.split(" ")) {
+            functionCall.getOutputCollector().add(new Tuple(word));
+        }
+    }
+
 }
