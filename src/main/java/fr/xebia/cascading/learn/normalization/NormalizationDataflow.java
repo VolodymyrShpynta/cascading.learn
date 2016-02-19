970f7481854f00c927a954781ab0015038579d4f
Volodymyr Shpynta <vshpynta@cogniance.com>
2016-02-20 01:18:21 +0200

    Adjusted implementation of data normalization

--- src/main/java/fr/xebia/cascading/learn/normalization/NormalizationDataflow.java
+++ src/main/java/fr/xebia/cascading/learn/normalization/NormalizationDataflow.java
@@ -1,6 +1,7 @@
 package fr.xebia.cascading.learn.normalization;
 
 import cascading.flow.FlowDef;
+import cascading.operation.expression.ExpressionFilter;
 import cascading.pipe.Each;
 import cascading.pipe.Pipe;
 import cascading.tap.Tap;
@@ -11,10 +12,11 @@ public class NormalizationDataflow {
 
     public static FlowDef normalize(Tap<?, ?, ?> simmonsSource, Tap<?, ?, ?> normalizedSink) {
         Pipe pipe = new Each("split", Fields.ALL, new CustomSplitFunction<>(new Fields("group", "node_type", "node_number", "node_name")), Fields.RESULTS);
+        ExpressionFilter nodeNameFilter = new ExpressionFilter("node_name.toLowerCase().contains(\"null\")", String.class);
+        pipe = new Each(pipe, new Fields("node_name"), nodeNameFilter);
         return FlowDef.flowDef()//
                 .addSource(pipe, simmonsSource) //
                 .addTail(pipe)//
                 .addSink(pipe, normalizedSink);
     }
-
 }
