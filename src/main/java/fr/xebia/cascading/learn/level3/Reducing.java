7ab65d501c1d895272d6b0673ea5f35e8b1d867e
VolodymyrShpynta <vshpynta@cogniance.com>
2016-02-19 14:49:22 +0200

    Fixed learnToAggregate test

--- src/main/java/fr/xebia/cascading/learn/level3/Reducing.java
+++ src/main/java/fr/xebia/cascading/learn/level3/Reducing.java
@@ -7,8 +7,10 @@ import cascading.operation.aggregator.Count;
 import cascading.pipe.Each;
 import cascading.pipe.Every;
 import cascading.pipe.GroupBy;
+import cascading.pipe.Pipe;
 import cascading.pipe.assembly.CountBy;
 import cascading.tap.Tap;
+import cascading.tuple.Fields;
 
 /**
  * Once each input has been individually curated, it can be needed to aggregate
@@ -21,13 +23,19 @@ public class Reducing {
      * that once grouped, the semantic is different. You will need to use a
      * {@link Every} instead of a {@link Each}. And {@link Count} is an
      * {@link Aggregator} instead of a {@link Function}.
-     * 
+     * <p/>
      * source field(s) : "word" sink field(s) : "word","count"
-     * 
+     *
      * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#_groupby
      */
     public static FlowDef aggregate(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
-        return null;
+        Pipe assembly = new Pipe("aggregate");
+        assembly = new GroupBy(assembly, new Fields("word"));
+        assembly = new Every(assembly, new Fields("word"), new Count(), Fields.ALL);
+        return FlowDef.flowDef()
+                .addSource(assembly, source)
+                .addTail(assembly)
+                .addSink(assembly, sink);
     }
 
     /**
@@ -35,9 +43,9 @@ public class Reducing {
      * technique almost similar to map/reduce 'combiner'. Use {@link CountBy} in
      * order to do the same thing as above. It is shorter to write and more
      * efficient.
-     * 
+     * <p/>
      * source field(s) : "word" sink field(s) : "word","count"
-     * 
+     *
      * @see http://docs.cascading.org/cascading/3.0/userguide/ch17-subassemblies.html#CountBy
      */
     public static FlowDef efficientlyAggregate(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
