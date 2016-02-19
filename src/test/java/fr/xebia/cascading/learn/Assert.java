61c4f7adc99f27f5e5402fb06b6ee128db8987f3
VolodymyrShpynta <vshpynta@cogniance.com>
2016-02-19 15:18:14 +0200

    Non functional changes

--- src/test/java/fr/xebia/cascading/learn/Assert.java
+++ src/test/java/fr/xebia/cascading/learn/Assert.java
@@ -11,15 +11,15 @@ import com.google.common.io.Files;
  * Shortcut to compare the content of two files.
  */
 public class Assert {
-	
-	public static void sameContent(String actualOutputPath, String expectedOuputPath) throws Exception {
-		String actualContent = contentOf(actualOutputPath);
-		String expectedContent = contentOf(expectedOuputPath);
-		Assertions.assertThat(actualContent).isEqualTo(expectedContent);
-	}
 
-	private static String contentOf(String filePath) throws Exception {
-		return Files.toString(new File(filePath), Charsets.UTF_8);
-	}
+    public static void sameContent(String actualOutputPath, String expectedOutputPath) throws Exception {
+        String actualContent = contentOf(actualOutputPath);
+        String expectedContent = contentOf(expectedOutputPath);
+        Assertions.assertThat(actualContent).isEqualTo(expectedContent);
+    }
+
+    private static String contentOf(String filePath) throws Exception {
+        return Files.toString(new File(filePath), Charsets.UTF_8);
+    }
 
 }
