970caa36aa5ee37cccf19575cee01594940f5af9
VolodymyrShpynta <vshpynta@cogniance.com>
2016-02-19 17:12:15 +0200

    Fixed learnToCoGroup test

--- src/main/java/fr/xebia/cascading/learn/level4/NonLinearDataflow.java
+++ src/main/java/fr/xebia/cascading/learn/level4/NonLinearDataflow.java
@@ -1,11 +1,8 @@
 package fr.xebia.cascading.learn.level4;
 
 import cascading.flow.FlowDef;
-import cascading.operation.expression.ExpressionFilter;
 import cascading.pipe.CoGroup;
-import cascading.pipe.Each;
 import cascading.pipe.Pipe;
-import cascading.pipe.assembly.Discard;
 import cascading.pipe.joiner.InnerJoin;
 import cascading.tap.Tap;
 import cascading.tuple.Fields;
@@ -15,39 +12,49 @@ import cascading.tuple.Fields;
  * be non linear, with multiples sources, multiples sinks, forks and merges.
  */
 public class NonLinearDataflow {
-	
-	/**
-	 * Use {@link CoGroup} in order to know the party of each presidents.
-	 * You will need to create (and bind) one Pipe per source.
-	 * You might need to correct the schema in order to match the expected results.
-	 * 
-	 * presidentsSource field(s) : "year","president"
-	 * partiesSource field(s) : "year","party"
-	 * sink field(s) : "president","party"
-	 * 
-	 * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#_cogroup
-	 */
-	public static FlowDef cogroup(Tap<?, ?, ?> presidentsSource, Tap<?, ?, ?> partiesSource,
-			Tap<?, ?, ?> sink) {
-		return null;
-	}
-	
-	/**
-	 * Split the input in order use a different sink for each party. There is no
-	 * specific operator for that, use the same Pipe instance as the parent.
-	 * You will need to create (and bind) one named Pipe per sink.
-	 * 
-	 * source field(s) : "president","party"
-	 * gaullistSink field(s) : "president","party"
-	 * republicanSink field(s) : "president","party"
-	 * socialistSink field(s) : "president","party"
-	 * 
-	 * In a different context, one could use {@link PartitionTap} in order to arrive to a similar results.
-	 * @see http://docs.cascading.org/cascading/3.0/userguide/ch15-advanced.html#partition-tap
-	 */
-	public static FlowDef split(Tap<?, ?, ?> source,
-			Tap<?, ?, ?> gaullistSink, Tap<?, ?, ?> republicanSink, Tap<?, ?, ?> socialistSink) {
-		return null;
-	}
-	
+
+    /**
+     * Use {@link CoGroup} in order to know the party of each presidents.
+     * You will need to create (and bind) one Pipe per source.
+     * You might need to correct the schema in order to match the expected results.
+     * <p/>
+     * presidentsSource field(s) : "year","president"
+     * partiesSource field(s) : "year","party"
+     * sink field(s) : "president","party"
+     *
+     * @see http://docs.cascading.org/cascading/3.0/userguide/ch05-pipe-assemblies.html#_cogroup
+     */
+    public static FlowDef cogroup(Tap<?, ?, ?> presidentsSource, Tap<?, ?, ?> partiesSource,
+                                  Tap<?, ?, ?> sink) {
+        Pipe presidentsPipe = new Pipe("presidentsPipe");
+        Pipe partiesPipe = new Pipe("partiesPipe");
+        Fields commonFields = new Fields("year");
+        Fields newFields = new Fields("year1", "president", "year2", "party");
+        Pipe joinedPipe = new CoGroup(presidentsPipe, commonFields, partiesPipe, commonFields, newFields, new InnerJoin());
+        return FlowDef.flowDef()
+                .addSource(presidentsPipe, presidentsSource)
+                .addSource(partiesPipe, partiesSource)
+                .addTail(joinedPipe)
+                .addSink(joinedPipe, sink);
+    }
+
+    /**
+     * Split the input in order use a different sink for each party. There is no
+     * specific operator for that, use the same Pipe instance as the parent.
+     * You will need to create (and bind) one named Pipe per sink.
+     * <p/>
+     * source field(s) : "president","party"
+     * gaullistSink field(s) : "president","party"
+     * republicanSink field(s) : "president","party"
+     * socialistSink field(s) : "president","party"
+     * <p/>
+     * In a different context, one could use {@link PartitionTap} in order to arrive to a similar results.
+     *
+     * @see http://docs.cascading.org/cascading/3.0/userguide/ch15-advanced.html#partition-tap
+     */
+    public static FlowDef split(Tap<?, ?, ?> source,
+                                Tap<?, ?, ?> gaullistSink, Tap<?, ?, ?> republicanSink, Tap<?, ?, ?> socialistSink) {
+        return null;
+    }
+
 }
--- src/test/java/fr/xebia/cascading/learn/level4/NonLinearDataflowTest.java
+++ src/test/java/fr/xebia/cascading/learn/level4/NonLinearDataflowTest.java
@@ -1,69 +1,99 @@
 package fr.xebia.cascading.learn.level4;
 
-import org.junit.Before;
-import org.junit.Test;
-
+import cascading.flow.Flow;
+import cascading.flow.FlowConnector;
 import cascading.flow.FlowDef;
 import cascading.flow.local.LocalFlowConnector;
+import cascading.scheme.Scheme;
 import cascading.scheme.local.TextDelimited;
 import cascading.tap.SinkMode;
 import cascading.tap.Tap;
 import cascading.tap.local.FileTap;
+import cascading.tuple.Fields;
 import fr.xebia.cascading.learn.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class NonLinearDataflowTest {
-	
-	@Before
-	public void doNotCareAboutOsStuff() {
-		System.setProperty("line.separator", "\n");
-	}
-	
-	@Test
-	public void learnToCoGroup() throws Exception {
-		// inputs of the job
-		String presidentsPath = "src/test/resources/fifth-france-presidents.txt";
-		Tap<?, ?, ?> presidentsSource = new FileTap(new TextDelimited(true, "\t"), presidentsPath);
-		
-		String partiesPath = "src/test/resources/fifth-france-parties.txt";
-		Tap<?, ?, ?> partiesSource = new FileTap(new TextDelimited(true, "\t"), partiesPath);
-		
-		// actual output of the job
-		String sinkPath = "target/level4/cogroup.txt";
-		Tap<?, ?, ?> sink = new FileTap(new TextDelimited(true, "\t"), sinkPath, SinkMode.REPLACE);
-		
-		// create the job definition, and run it
-		FlowDef flowDef = NonLinearDataflow.cogroup(presidentsSource, partiesSource, sink);
-		new LocalFlowConnector().connect(flowDef).complete();
-		
-		// check that actual and expect outputs are the same
-		Assert.sameContent(sinkPath, "src/test/resources/level4/cogroup/expectation.txt");
-	}
-	
-	@Test
-	public void learnToSplit() throws Exception {
-		// input of the job
-		String sourcePath = "src/test/resources/level4/cogroup/expectation.txt";
-		Tap<?, ?, ?> source = new FileTap(new TextDelimited(true, "\t"), sourcePath);
-		
-		// actual outputs of the job
-		String gaullistPath = "target/level4/split-gaullist.txt";
-		Tap<?, ?, ?> gaullistSink = new FileTap(new TextDelimited(true, "\t"), gaullistPath, SinkMode.REPLACE);
-		
-		String republicanPath = "target/level4/split-republican.txt";
-		Tap<?, ?, ?> republicanSink = new FileTap(new TextDelimited(true, "\t"), republicanPath, SinkMode.REPLACE);
-		
-		String socialistPath = "target/level4/split-socialist.txt";
-		Tap<?, ?, ?> socialistSink = new FileTap(new TextDelimited(true, "\t"), socialistPath, SinkMode.REPLACE);
-		
-		// create the job definition, and run it
-		FlowDef flowDef = NonLinearDataflow.split(source, gaullistSink, republicanSink, socialistSink);
-		new LocalFlowConnector().connect(flowDef).complete();
-		
-		// check that actual and expect outputs are the same
-		Assert.sameContent(gaullistPath, "src/test/resources/level4/split/expectation-gaullist.txt");
-		Assert.sameContent(republicanPath, "src/test/resources/level4/split/expectation-republican.txt");
-		Assert.sameContent(socialistPath, "src/test/resources/level4/split/expectation-socialist.txt");
 
-	}
+    @Before
+    public void doNotCareAboutOsStuff() {
+        System.setProperty("line.separator", "\n");
+    }
+
+    @Test
+    public void learnToCoGroup() throws Exception {
+        // inputs of the job
+        String presidentsPath = "src/test/resources/fifth-france-presidents.txt";
+        Tap<?, ?, ?> presidentsSource = new FileTap(new TextDelimited(true, "\t"), presidentsPath);
+
+        String partiesPath = "src/test/resources/fifth-france-parties.txt";
+        Tap<?, ?, ?> partiesSource = new FileTap(new TextDelimited(true, "\t"), partiesPath);
+
+        // actual output of the job
+        String sinkPath = "target/level4/cogroup.txt";
+        Scheme sinkScheme = new TextDelimited(new Fields("president", "party"), true, "\t");
+        Tap<?, ?, ?> sink = new FileTap(sinkScheme, sinkPath, SinkMode.REPLACE);
+
+        // create the job definition, and run it
+        FlowDef flowDef = NonLinearDataflow.cogroup(presidentsSource, partiesSource, sink);
+        new LocalFlowConnector().connect(flowDef).complete();
+
+        // check that actual and expect outputs are the same
+        Assert.sameContent(sinkPath, "src/test/resources/level4/cogroup/expectation.txt");
+    }
+
+    @Test
+    public void myLearnToCoGroup() throws Exception {
+        // inputs of the job
+        String presidentsPath = "src/test/resources/fifth-france-presidents.txt";
+        Scheme presidentsSchemeDefinition = new TextDelimited(new Fields("year", "president"), true, "\t");
+        Tap<?, ?, ?> presidentsSource = new FileTap( presidentsSchemeDefinition, presidentsPath );
+
+        String partiesPath = "src/test/resources/fifth-france-parties.txt";
+        Scheme partiesSchemeDefinition = new TextDelimited(new Fields("year", "party"), true, "\t");
+        Tap<?, ?, ?> partiesSource = new FileTap( partiesSchemeDefinition, partiesPath );
+
+        // actual output of the job
+        String sinkPath = "target/level4/cogroup.txt";
+        Scheme sinkScheme = new TextDelimited(new Fields("president", "party"), true, "\t");
+        Tap<?, ?, ?> sink = new FileTap( sinkScheme, sinkPath, SinkMode.REPLACE );
+
+        FlowDef flowDef = NonLinearDataflow.cogroup(presidentsSource, partiesSource, sink);
+        FlowConnector flowConnector = new LocalFlowConnector();
+        Flow flow = flowConnector.connect(flowDef);
+        flow.complete();
+        //OPTIONAL: Flow diagram
+        flow.writeDOT(sinkPath + "target/level4/flowdiagram3.dot");
+
+        Assert.sameContent(sinkPath, "src/test/resources/level4/cogroup/expectation.txt");
+    }
+
+    @Test
+    public void learnToSplit() throws Exception {
+        // input of the job
+        String sourcePath = "src/test/resources/level4/cogroup/expectation.txt";
+        Tap<?, ?, ?> source = new FileTap(new TextDelimited(true, "\t"), sourcePath);
+
+        // actual outputs of the job
+        String gaullistPath = "target/level4/split-gaullist.txt";
+        Tap<?, ?, ?> gaullistSink = new FileTap(new TextDelimited(true, "\t"), gaullistPath, SinkMode.REPLACE);
+
+        String republicanPath = "target/level4/split-republican.txt";
+        Tap<?, ?, ?> republicanSink = new FileTap(new TextDelimited(true, "\t"), republicanPath, SinkMode.REPLACE);
+
+        String socialistPath = "target/level4/split-socialist.txt";
+        Tap<?, ?, ?> socialistSink = new FileTap(new TextDelimited(true, "\t"), socialistPath, SinkMode.REPLACE);
+
+        // create the job definition, and run it
+        FlowDef flowDef = NonLinearDataflow.split(source, gaullistSink, republicanSink, socialistSink);
+        new LocalFlowConnector().connect(flowDef).complete();
+
+        // check that actual and expect outputs are the same
+        Assert.sameContent(gaullistPath, "src/test/resources/level4/split/expectation-gaullist.txt");
+        Assert.sameContent(republicanPath, "src/test/resources/level4/split/expectation-republican.txt");
+        Assert.sameContent(socialistPath, "src/test/resources/level4/split/expectation-socialist.txt");
+
+    }
 
 }
